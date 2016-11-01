bffc0bff2f9a5aec6417e22eaadb5b7ac2346c6d
Robert Christian <rchristian@truecar.com>
2016-11-01 13:55:39 -0700

    Separate machine from interface.

---
+++ machine.js
@@ -0,0 +1,63 @@
+
+/* State machine for blinking LED's */
+
+var old_state = 0;
+var new_state = 300;
+var delta_rate = 2 / 10 * 1000;
+var t = Date.now();
+// var max;
+
+
+function setup(opts) {
+
+  if (opts.delta) {
+    delta_rate = opts.delta * 1000;
+  }
+}
+
+function getDelta(time) {
+    return delta_rate * time;
+}
+
+
+function setRate(input) {
+  new_state = parseInt(input, 10);
+}
+
+
+function targetHasChanged() {
+
+    return new_state !== old_state;
+}
+
+
+function applyStateChanges() {
+
+    var now = Date.now();
+    var p_delta = getDelta(now - t);
+
+    if (new_state > old_state) {
+        old_state = (old_state + p_delta > new_state) ? new_state : old_state + p_delta;
+    }
+    else {
+        old_state = (old_state - p_delta < new_state) ? new_state : old_state - p_delta;
+    }
+    
+    t = now;
+}
+
+
+function step() {
+
+  if (targetHasChanged()) {
+    applyStateChanges();
+    return state;
+  }
+}
+
+
+module.exports = {
+  setRate: setRate,
+  setup: setup,
+  step: step,
+}
--- main.js
+++ main.js
@@ -3,65 +3,28 @@
 
 var five = require("johnny-five");
 var board = new five.Board();
+var machine = require("./machine.js");
 
 var led;
-var old_state = 0;
-var new_state = 300;
-var delta_rate = 2 / 10 * 1000;
-var t = Date.now();
 
-
-function getDelta(time) {
-    return delta_rate * time;
-}
+machine.setup({
+  delta: 2 / 10,
+  max: 2,
+});
 
 
 board.on("ready", function() {
 	led = new five.Led(13);
 
 	this.repl.inject({
-		rate: setRate
+		rate: machine.setRate
 	});
 
-	setInterval(loop, 0);
+	setInterval(function(){
+    led.blink(machine.step())
+  }, 0);
 });
 
 board.on("exit", function() {
 	led.off();
 });
-
-
-function setRate(input) {
-	new_state = parseInt(input, 10);
-}
-
-
-function targetHasChanged() {
-    return new_state !== old_state;
-}
-
-
-function applyStateChanges() {
-
-    var now = Date.now();
-    var p_delta = getDelta(now - t);
-
-    if (new_state > old_state) {
-        old_state = (old_state + p_delta > new_state) ? new_state : old_state + p_delta;
-    }
-    else {
-        old_state = (old_state - p_delta < new_state) ? new_state : old_state - p_delta;
-    }
-    
-    t = now;
-}
-
-
-function loop() {
-
-	if (targetHasChanged()) {
-		applyStateChanges();
-		led.blink(state);
-	}
-
-}
